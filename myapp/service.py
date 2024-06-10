8096b22d326e816d40b0d7cc1c7d2f1973c09738
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-10 23:40:05 +0700

    #code

--- myapp/service.py
+++ myapp/service.py
@@ -1,8 +1,10 @@
 from modules.model_modules import get_model
 
+
 model = get_model("") # init by dev
 
 def s2t_sevice(file):
   # init model
+    
   prediction = model.predict() #######
   return file
