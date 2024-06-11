b9a0723dcc81be98a5eb65c7c756843dc8ab28f8
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-11 15:03:07 +0700

    #code

--- core/modules/data_pipeline.py
+++ core/modules/data_pipeline.py
@@ -1,18 +1,25 @@
 import torchaudio
 import torch
 
+
 class DataPipeline:
     def __init__(self, processor) -> None:
         super.__init__(processor=processor)
         self.processor = processor
 
+    def _load_audio_data(self, input):
+        audio_array, _ = torchaudio.load(input)
+        return audio_array
+
     def pipeline_s2t(self, input):
         try:
-            preprocessed_data = None # preprocessed_data
-            audio_array, _ = torchaudio.load(input) # audio array
-            audio_array = torch.mean(audio_array, dim=0) # take mean where audio has 2 dimensions
+            preprocessed_data = None  # preprocessed_data
+            audio_array = self._load_audio_data(input=input)  # audio array
+            audio_array = torch.mean(
+                audio_array, dim=0
+            )  # take mean where audio has 2 dimensions
             preprocessed_data = self.processor(audio_array)
-            return preprocessed_data      
+            return preprocessed_data
         except Exception as e:
             raise ValueError(e)
 
--- core/modules/model_modules.py
+++ core/modules/model_modules.py
@@ -1,6 +1,6 @@
 import torch
 from omegaconf import DictConfig
-from transformers import AutoProcessor
+from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq, pipeline
 from .data_pipeline import DataPipeline
 
 
@@ -8,15 +8,18 @@ class ModelModules:
     def __init__(self, conf: DictConfig) -> None:
         self.conf = conf
         self.model = self.get_model()
+        self.processor = self.get_pretrained_processor()
         self.data_pipeline = DataPipeline(
-            self.get_pretrained_processor()
+            processor=self.processor
         )  # get processor directly
 
     def get_pretrained_processor(self):
         _, processor = self._get_pretrained_model()
         return processor
 
+    @staticmethod
     def _get_pretrained_model(model_name: str, use_nemo: bool):
+        torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
         # init
         model = None
         processor = None
@@ -25,7 +28,14 @@ class ModelModules:
         if use_nemo:
             # nemo pipeline
             pass
-
+        else:
+            model = AutoModelForSpeechSeq2Seq.from_pretrained(
+                model_name,
+                torch_dtype=torch_dtype,
+                low_cpu_mem_usage=True,
+                use_safetensors=True,
+            )
+            processor = AutoProcessor.from_pretrained(model_name)
         return model, processor
 
     def get_model(self):
@@ -40,6 +50,30 @@ class ModelModules:
         return model
 
     def s2t_predict(self, input: torch.Tensor):
-        processed_input = self.data_pipeline(input)
-        out = self.model(processed_input)  # transcbie with nemo orelse
-        return out
+        processed_input = None
+        output = None
+        if self.conf.pretrained_section.use_nemo == False:
+            # init
+            torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+            device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+            processed_input = self.data_pipeline._load_audio_data(input=input)
+
+            params = {
+                "task": "automatic-speech-recognition",
+                "model": self.model,
+                "tokenizer": self.processor.tokenizer,
+                "feature_extractor": self.processor.feature_extractor,
+                "max_new_tokens": 128,
+                "torch_dtype": torch_dtype,
+                "device": device,
+            }
+
+            pipe = pipeline(**params)  # pipeline for processing input
+
+            output = pipe(processed_input)
+
+        elif self.conf.pretrained_section.use_nemo == False:
+            processed_input = self.data_pipeline(input)
+            output = self.model(processed_input)  # transcbie with nemo orelse
+        return output
