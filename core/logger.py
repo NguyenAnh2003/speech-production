70eac93d76f5c58162c097d6e3c467705a70ae74
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-11 10:00:22 +0700

    .

--- core/logger.py
+++ core/logger.py
@@ -1,13 +1,12 @@
 import logging
 
 
-def setup_logger(path: str = "./logs/example.log", location: str = ""):
+def setup_logger():
     logger = logging.getLogger(__name__)
     handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        f"{location}: %(levelname)s: -- %(message)s --: %(asctime)s"
-    )
+    formatter = logging.Formatter("%(levelname)s: -- %(message)s --: %(asctime)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+    logger.removeHandler(logging.FileHandler)  # remove file handler
     logger.setLevel(logging.DEBUG)
     return logger
