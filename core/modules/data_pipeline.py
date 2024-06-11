d3afaf7670f3eb82d9e363d341932365658a8984
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-06-11 14:35:04 +0700

    #code

--- core/modules/data_pipeline.py
+++ core/modules/data_pipeline.py
@@ -1,5 +1,5 @@
 import torchaudio
-
+import torch
 
 class DataPipeline:
     def __init__(self, processor) -> None:
@@ -9,7 +9,8 @@ class DataPipeline:
     def pipeline_s2t(self, input):
         try:
             preprocessed_data = None # preprocessed_data
-            audio_array = torchaudio.load(input) # audio array
+            audio_array, _ = torchaudio.load(input) # audio array
+            audio_array = torch.mean(audio_array, dim=0) # take mean where audio has 2 dimensions
             preprocessed_data = self.processor(audio_array)
             return preprocessed_data      
         except Exception as e:
